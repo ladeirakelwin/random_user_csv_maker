02edf5d5c12867f2bdfac6f1fc6875927afb0047
ladeirakelwin <ladeirakelwin@gmail.com>
2020-08-30 21:10:42 -0300

    gerador de usuario json2csv

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const fs = require('fs')
 const geraUsuario = async () => {
   const response = await fetch('https://randomuser.me/api/?results=10')
   const json = await response.json()
-  const data = json.results
+  const datum = json.results
 
-  data.forEach((user, index) => console.log(user))
+  datum.forEach(({
+    name,
+    email,
+    dob,
+    gender,
+    login
+  },index) => {
+    const fields = "\n"+name.first + "," + name.last + "," +
+      email + "," + dob.age + "," + gender + "," +
+      login.username + "," + login.sha256 + ","
+    fs.appendFile('dados/usuario.csv', fields, (err) => {
+      if (err) throw err
+    })
+  })
+  console.log('Terminado!')
 }
 
 geraUsuario()
