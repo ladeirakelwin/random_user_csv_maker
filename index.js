2326f8a613891e6018e125fec06d14f95b85ceb2
ladeirakelwin <ladeirakelwin@gmail.com>
2020-08-30 21:30:11 -0300

    csv preenchido e tratamento de erro

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 const geraUsuario = async () => {
-  const response = await fetch('https://randomuser.me/api/?results=10')
-  const json = await response.json()
-  const datum = json.results
+    try {
+    const response = await fetch('https://randomuser.me/api/?results=5000')
+    const json = await response.json()
+    const datum = json.results
 
-  datum.forEach(({
-    name,
-    email,
-    dob,
-    gender,
-    login
-  },index) => {
-    const fields = "\n"+name.first + "," + name.last + "," +
-      email + "," + dob.age + "," + gender + "," +
-      login.username + "," + login.sha256 + ","
-    fs.appendFile('dados/usuario.csv', fields, (err) => {
-      if (err) throw err
+    datum.forEach(({
+      name,
+      email,
+      dob,
+      gender,
+      login
+    }, index) => {
+      const fields = "\n" + name.first + "," + name.last + "," +
+        email + "," + dob.age + "," + gender + "," +
+        login.username + "," + login.sha256 + ","
+      fs.appendFile('dados/usuario.csv', fields, (err) => {
+        if (err) throw err
+      })
     })
-  })
-  console.log('Terminado!')
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 geraUsuario()
